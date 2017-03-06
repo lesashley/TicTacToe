fd21ee42ba28ec3a551f955cce0c5a69bca95312
lesashley <lesashley56@gmail.com>
2017-03-05 23:24:04 -0500

    Agregando botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
-//alert("svdfbdgb");
-function ticTacToe() {
+function ticTacToe(boton) {
 
+
+
+  var boton1 = document.getElementById("1");
+  var boton2 = document.getElementById("2");
+  var boton3 = document.getElementById("3");
+  var boton4 = document.getElementById("4");
+  var boton5 = document.getElementById("5");
+  var boton6 = document.getElementById("6");
+  var boton7 = document.getElementById("7");
+  var boton8 = document.getElementById("8");
+  var boton9 = document.getElementById("9");
 }
 
 if(typeof exports !== 'undefined') {
